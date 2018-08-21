e75c1c6fd7163371e2671f156e025d0ac5679bae
Tyler Quiring <tylerdq@gmail.com>
2018-08-20 22:11:07 -0400

    Most basic function (read and print first page) working again

--- pdfda.py
+++ pdfda.py
@@ -1,21 +1,13 @@
 import PyPDF2 as pyp
-import glob
+#import os
 
-pdfs = []
-pnums = []
-for filename in glob.glob("*.pdf"):
-    with open(filename, 'rb') as f:
-        pdfs.append(f)
-        pdfReader = pyp.PdfFileReader(f)
-        pnums.append(pdfReader.getNumPages)
-#for f in pdfs:
-#    pdfFileObj = open(filename, 'rb')
-#    pdfReader = pyp.PdfFileReader(pdfFileObj)
-#    pnums.append(pdfFileObj.getNumPages)
+pages = []
 
-print(pnums)
+file = ('r15d.pdf')
+read_pdf = pyp.PdfFileReader(file)
+pnums = read_pdf.getNumPages()
 
-#for pnum in range(pnums):   # use xrange in Py2
-#    page = read_pdf.getPage(page_number).extractText().split(" ")  # Extract page wise text then split based on spaces as required by you
-#    pages.append(page)
-#print(pages[0])
+for pnum in range(pnums):
+    page = read_pdf.getPage(pnum).extractText().split(" ")
+    pages.append(page)
+print(pages[0])
