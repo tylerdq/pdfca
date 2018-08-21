acff91cdbe038a4985fe298a3bb8905132676327
Tyler Quiring <tylerdq@gmail.com>
2018-08-21 09:23:37 -0400

    Make work pulling all pdfs and writing their names to a header string
    Rest of script is a jumble of commented and uncommented partially-working stuff

--- pdfda.py
+++ pdfda.py
@@ -1,41 +1,49 @@
+import sys
 import PyPDF2 as pyp
-#import os
+import os
 #import pandas as pd
 import csv
 
-pages,digit,rhetor = [], [], []
-dfkeys = ['p#', 'digit#', 'rhetor#']
+word = sys.argv[1]
+
+pages = []
+#pages,digit,rhetor = [], [], []
+head = ['p#']
 words = ['digit', 'rhetor']
-wordvars = [digit, rhetor]
-worddict = dict(zip(words, wordvars))
+#wordvars = [digit, rhetor]
+#worddict = dict(zip(words, wordvars))
 
-file = ('r15d.pdf')
-read_pdf = pyp.PdfFileReader(file)
-pnum = read_pdf.getNumPages()
-pnums = []
+#file = ('r15d.pdf')
+for file in os.listdir(os.getcwd()):
+    if file.endswith(".pdf"):
+        head.append(file[:-4])
+        read_pdf = pyp.PdfFileReader(file)
+        pnum = read_pdf.getNumPages()
+        pnums = []
+print(head)
 
-for p in range(pnum):
-    text = read_pdf.getPage(p).extractText().split(" ")
-    pages.append(text)
-    pnums.append(p+1)
-#print(pages[0])
+#    for p in range(pnum):
+#        text = read_pdf.getPage(p).extractText().split(" ")
+#        pages.append(text)
+#        pnums.append(p+1)
+    #print(pages[0])
 
-for page in pages:
-    for k, i in worddict.items():
-        i.append(sum(k in w for w in page))
+#    for page in pages:
+#        for k, i in worddict.items():
+#            i.append(sum(k in w for w in page))
 
 dfvalues = [pnums, digit, rhetor]
 
-result = dict(zip(dfkeys, dfvalues))
+result = dict(zip(head, dfvalues))
 
 with open('output.csv', mode='w') as outfile:
     out_writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    out_writer.writerow(dfkeys)
+    out_writer.writerow(head)
     out_writer.writerows(zip(*result.values()))
 
 exit()
 
-#result = dict(zip(dfkeys, dfvalues))
+#result = dict(zip(head, dfvalues))
 #df = pd.DataFrame(data=result)
 #print(df)
