4094e76c55109d3cfd6bce1156595a7099b45d75
Tyler Quiring <tylerdq@gmail.com>
2018-08-20 22:32:32 -0400

    Get word counts by pdf page half-working

--- pdfda.py
+++ pdfda.py
@@ -2,6 +2,7 @@ import PyPDF2 as pyp
 #import os
 
 pages = []
+words = ['digital','rhetoric']
 
 file = ('r15d.pdf')
 read_pdf = pyp.PdfFileReader(file)
@@ -10,4 +11,16 @@ pnums = read_pdf.getNumPages()
 for pnum in range(pnums):
     page = read_pdf.getPage(pnum).extractText().split(" ")
     pages.append(page)
-print(pages[0])
+#print(pages[0])
+
+counter = []
+
+for p in pages:
+    for word in words:
+        counter.append(p.count(word))
+
+print(counter)
+
+#for word in words:
+#    (w, pages.count(w)) for w in set(pages)
+#print
