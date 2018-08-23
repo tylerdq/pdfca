62ae5c0e9dc8d21e790a5d75aa373147fac12ef9
Tyler Quiring <tylerdq@gmail.com>
2018-08-23 18:56:08 -0400

    Got the output dict working properly
    This was using the solution given by Sikhur on Codementor.
    
    Next, the output csv files must be generated properly.

--- pdfda.py
+++ pdfda.py
@@ -1,16 +1,15 @@
 import sys
 import PyPDF2 as pyp
 import os
-#import pandas as pd
-#from collections import defaultdict as dfd
 import itertools
 import csv
 
 #word = sys.argv[1]
 ndict = {}  # Preallocate empty dict for number of pages per PDF
 pdict = {}  # Preallocate empty dict for page contents per PDF
-cdict = {}  # Preallocate empty dict for term counts per PDF
-words = open(sys.argv[1], 'rt')  # Create variable for input file
+#cdict = {}  # Preallocate empty dict for term counts per PDF
+with open(sys.argv[1], 'rt') as input_file: # Create variable for input file
+    words = input_file.read().splitlines()
 
 os.chdir('input')  # Change directory to location of PDFs
 
@@ -20,23 +19,35 @@ os.chdir('input')  # Change directory to location of PDFs
 print('Looking for PDFs to scrape...')
 for file in os.listdir('.'):  # Loop through all files in input folder
     if file.endswith('.pdf'):  # Filter only PDFs
-        f = file[:-4]  # Trim extension from filename
-        ndict[f] = []  # Create file keys in dict with empty list for each
-        pdict[f] = {}  # Create file keys in dict with empty dict for each
+        file_stem = file[:-4]  # Trim extension from filename
+        #ndict[f] = []  # Create file keys in dict with empty list for each
+        pdict[file_stem] = {}  # Create file keys in dict with empty dict for each
         #cdict[f] = []
         read_pdf = pyp.PdfFileReader(file)  # Name reader for current PDF
-        ndict[f] = read_pdf.getNumPages()  # Count pages in current PDF
-        print('--- ' + str(ndict[f]) + ' pages in ' + str(file))  # Display progress
+        ndict[file_stem] = read_pdf.getNumPages()  # Count pages in current PDF
+        print('--- ' + str(ndict[file_stem]) + ' pages in ' + str(file))  # Display progress
 
-        for f in ndict.keys():
-            for p in range(ndict[f]):  # Loop through pages to extract text
+        for key_name in ndict.keys():
+            for page_number in range(ndict[key_name]):  # Loop through pages to extract text
                 #pdict[f][p] = []  # Preallocate empty list for page contents
-                #text = read_pdf.getPage(p).extractText().lower().split(" ")
-                pdict[f][p] = read_pdf.getPage(p).extractText().lower().strip().split(" ")  # Save extracted and split text
+                #text = read_pdf.getPage(p).extractText().lower().strip().split(" ")
+                text = read_pdf.getPage(page_number).extractText().strip().lower().split(" ")
+                pdict[key_name][page_number] = text  # Save extracted and split text
 
-print(pdict)
+os.chdir('../output')  # Change to output directory
 
-#os.chdir('../output')  # Change directory to where csv will be saved
+output = {}
+for filename, contents in pdict.items():
+    output[filename] = {}
+
+    for page_number, words_list in contents.items():
+        output[filename][page_number] = {}
+
+        for word in words:
+            word_stripped = word.strip()
+            output[filename][page_number][word_stripped] = sum(word_stripped in sub_word for sub_word in words_list)
+
+print(output)
 
 #print('Searching for words to count...')
 #for word in words:  # Loop through words in input file
