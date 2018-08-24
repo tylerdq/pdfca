8c9afcee50b70bf5c2d9cc1d0d53b701948be585
Tyler Quiring <tylerdq@gmail.com>
2018-08-23 20:42:00 -0400

    Add basic CSV export functionality
    Following the top solution in this StackExchange post (https://stackoverflow.com/questions/24334706/how-to-save-multi-layered-dictionary-to-csv), I added a few lines that iterate through the dict and write it to a CSV. However, the format is now how I want it (prefer to have multiple CSV files, one per term, each with a column per file, and the page numbers and term counts by page on rows).

--- pdfda.py
+++ pdfda.py
@@ -1,42 +1,30 @@
-import sys
-import PyPDF2 as pyp
-import os
-import itertools
-import csv
+import sys, os, PyPDF2, itertools, csv
 
-#word = sys.argv[1]
-ndict = {}  # Preallocate empty dict for number of pages per PDF
 pdict = {}  # Preallocate empty dict for page contents per PDF
-#cdict = {}  # Preallocate empty dict for term counts per PDF
-with open(sys.argv[1], 'rt') as input_file: # Create variable for input file
-    words = input_file.read().splitlines()
+with open(sys.argv[1], 'rt') as input_file:  # Create variable for input file
+    words = input_file.read().splitlines()  # Create list with all input words
 
 os.chdir('input')  # Change directory to location of PDFs
 
-#for word in words:
-#    word = word.strip()
-#    print('Searching for ' + '"' + word + '"' + ' in files...')
 print('Looking for PDFs to scrape...')
 for file in os.listdir('.'):  # Loop through all files in input folder
     if file.endswith('.pdf'):  # Filter only PDFs
         file_stem = file[:-4]  # Trim extension from filename
-        #ndict[f] = []  # Create file keys in dict with empty list for each
-        pdict[file_stem] = {}  # Create file keys in dict with empty dict for each
-        #cdict[f] = []
-        read_pdf = pyp.PdfFileReader(file)  # Name reader for current PDF
-        ndict[file_stem] = read_pdf.getNumPages()  # Count pages in current PDF
-        print('--- ' + str(ndict[file_stem]) + ' pages in ' + str(file))  # Display progress
+        filename = str(file)
+        pdict[filename] = {}  # Create empty sub-dict for each file
+        read_pdf = PyPDF2.PdfFileReader(file)  # Name reader for current PDF
+        pnums = read_pdf.getNumPages()  # Count pages in current PDF
+        print('--- ' + str(pnums) + ' pages in ' + str(file))
 
-        for key_name in ndict.keys():
-            for page_number in range(ndict[key_name]):  # Loop through pages to extract text
-                #pdict[f][p] = []  # Preallocate empty list for page contents
-                #text = read_pdf.getPage(p).extractText().lower().strip().split(" ")
-                text = read_pdf.getPage(page_number).extractText().strip().lower().split(" ")
-                pdict[key_name][page_number] = text  # Save extracted and split text
+        for pnum in range(pnums):
+            text = read_pdf.getPage(pnum).extractText().strip().lower().split(" ")  # Extract and pre-process text
+            pdict[filename][pnum] = text  # Write page text (merge with above?)
 
 os.chdir('../output')  # Change to output directory
 
 output = {}
+
+print('Searching for words to count...')
 for filename, contents in pdict.items():
     output[filename] = {}
 
@@ -47,24 +35,27 @@ for filename, contents in pdict.items():
             word_stripped = word.strip()
             output[filename][page_number][word_stripped] = sum(word_stripped in sub_word for sub_word in words_list)
 
-print(output)
+text = 'file,page,term,count\n'
+for filename in output:
+    pageData = output[filename]
+    for page in pageData:
+        termCount = pageData[page]
+        for term in termCount:
+            count = termCount[term]
+            text += filename + ',' + str(page) + ',' + term + ',' + str(count) + '\n'
 
-#print('Searching for words to count...')
-#for word in words:  # Loop through words in input file
-#    word = word.strip()  # Remove whitespace from words in input file
-#    print('--- Counting ' + '"' + word + '"' + '...')
-#    for k, v in pdict.items():  # Loop through page contents for each file
-#        cdict[k] = []
-#        for n, p in v.items():
-#            cdict[k].append(sum(word in w for w in p))  # Count now
-#        print('--- ' + 'Counted ' + word + ' in ' + k)  # Display progress
-#        print(cdict[k])
+f = open('output.csv', 'w')
+f.write(text)
+f.close()
 
-#    with open('output_' + word + '.csv', mode='w') as outfile:  #
-#        out_writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#print(output)
 
-#        out_writer.writerow(cdict.keys())  # Write the header row
-#        out_writer.writerows(itertools.zip_longest(*cdict.values()))
+#for word in words:
+#    with open(word + '.csv', mode='wb') as outfile:
+#        out_writer = csv.DictWriter(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+#        out_writer.writerow(output.keys())  # Write the header row
+#        out_writer.writerows(itertools.zip_longest(*output.values()))
 
 print('Done!')
 
